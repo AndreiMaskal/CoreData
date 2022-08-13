cc0ee0942715544a7cabfd5ce868ea0f113d1826
Andrik <masmay.pl@gmail.com>
2022-08-13 12:38:10 +0200

    setup rootViewController

--- CoreDataHW26/CoreDataHW26/ViewController.swift
+++ CoreDataHW26/CoreDataHW26/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        
     }
 
 
