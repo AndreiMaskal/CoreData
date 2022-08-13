0419d285828b05ad1fc1ceac8d751be7d77df631
Andrik <masmay.pl@gmail.com>
2022-08-13 12:19:04 +0200

    initial commit

---
+++ CoreDataHW26/CoreDataHW26/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataHW26
+//
+//  Created by Andrei Maskal on 13/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
